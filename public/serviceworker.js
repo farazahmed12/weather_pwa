ba60daf4e83398ba5d1d6ac013fb2921e665b16f
farazahmed12 <masterdot707@gmail.com>
2022-03-01 23:26:28 +0500

    Added manifest file and offline file

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -14,7 +14,28 @@ self.addEventListener("install", (event) => {
 });
 
 // Listen for Request
-self.addEventListener("install", (event) => {});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
 
 // Activate the SW
-self.addEventListener("install", (event) => {});
+self.addEventListener("activate", (event) => {
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhiteList.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
